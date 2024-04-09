de9fd97ca95ad9bfc138db5c3fdf721807aa164d
Ilian Kukov (ext.) <ilian.kukov@partner.doubleslash.de>
2024-04-09 23:35:49 +0300

    init handlebars

--- 06.ExpressTemplating/src/index.js
+++ 06.ExpressTemplating/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
+const handlebards = require('express-handlebards');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
-app.get('/',(req, res)=> {
-    res.send('Home page');
+app.engine('hbs', handlebards.engine({
+    extname: 'hbs',
+})).set('views',path.join(__dirname, 'views'));
+
+app.get('/',(req, res) => {
+    res.render('home',{layout: false});
 });
 
 app.listen(port,
